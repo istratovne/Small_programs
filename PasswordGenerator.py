e9a4fe2a1c6d890fcfce7f783c49bccda43c424a
istratovne <107531555+istratovne@users.noreply.github.com>
2022-07-20 16:12:33 +0300

    Update PasswordGenerator.py

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -5,6 +5,7 @@ LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
 UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 PUNCTUATION = '!#$%&*+-=?@^_'
 
+
 def info_request():
     while True:
         try: 
@@ -14,7 +15,6 @@ def info_request():
             print('Введите целое число')
         except ValueError:
             print('Введите целое число')
-
     while True:
         try:
             pass_len = int(input('Укажите длину одного пароля '))
@@ -23,44 +23,37 @@ def info_request():
             print('Введите целое число')
         except ValueError:
             print('Введите целое число')
-
     while True:
         include_digits = input('Включать ли в пароль цифры от 0 до 9?(y/n) ').lower()
         if not include_digits == 'y' and not include_digits == 'n':
             print('Введите ответ y или n')
         else:
             break
-
     while True:        
         include_uppercase = input('Включать ли в пароль прописные буквы?(y/n) ').lower()
         if not include_uppercase == 'y' and not include_uppercase == 'n':
             print('Введите ответ y или n')
         else:
             break
-
     while True:    
         include_lowercase = input('Включать ли в пароль строчные буквы?(y/n) ').lower()
         if not include_lowercase == 'y' and not include_lowercase == 'n':
             print('Введите ответ y или n')
         else:
             break
-
     while True:        
         include_punctuation = input('Включать ли символы !#$%&*+-=?@^_? (y/n) ').lower()
         if not include_punctuation == 'y' and not include_punctuation == 'n':
             print('Введите ответ y или n')
         else:
             break
-
     while True:        
         exception = input('Исключать ли неоднозначные символы il1Lo0O? (y/n) ').lower()
         if not exception == 'y' and not exception == 'n':
             print('Введите ответ y или n')
         else:
             break
-
-    chars = ''
-    
+    chars = '' 
     if include_digits == 'y':
         chars += DIGITS
     if include_uppercase == 'y':
@@ -71,9 +64,9 @@ def info_request():
         chars += PUNCTUATION
     if exception == 'y':
         for i in 'il1Lo0O':
-            chars = chars.replace(i, '')
-            
+            chars = chars.replace(i, '')        
     return pass_amount, pass_len, chars
+
 
 def generate_password():
     pass_amount, pass_len, chars = info_request()
